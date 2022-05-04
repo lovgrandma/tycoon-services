5fa9197b7b5757c22b7b598d9b3b12f49f470c58
Jesse <jessealexanderthompson@gmail.com>
2022-05-04 02:00:20 -0400

    Init mongo connection

--- sms/queue/sms_queue.go
+++ sms/queue/sms_queue.go
@@ -1,5 +1,25 @@
 package sms_queue
 
 import (
+	"log"
 	"github.com/go-bongo/bongo"
+	"tycoon.systems/tycoon-services/s3credentials"
 )
+
+var (
+	config = &bongo.Config{
+		ConnectionString: s3credentials.GetS3Data("mongo", "addressAuth", ""),
+		Database:         s3credentials.GetS3Data("mongo", "u", ""),
+	}
+	connection, err = bongo.Connect(config)
+)
+
+func main() {
+
+}
+
+func provisionSmsJob() {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- sms_server.go
+++ sms_server.go
@@ -5,7 +5,9 @@ import (
 	"log"
 	"github.com/google/uuid"
 	"net"
+	"reflect"
 	"tycoon.systems/tycoon-services/s3credentials"
+	"errors"
 
 	pb "tycoon.systems/tycoon-services/sms"
 	"google.golang.org/grpc"
@@ -19,12 +21,24 @@ type SmsManagementServer struct {
 	pb.UnimplementedSmsManagementServer
 }
 
+/* Determine if request to create Sms blast is genuine and if user has permissions. Attempt provision for job */
 func (s *SmsManagementServer) CreateNewSmsBlast(ctx context.Context, in *pb.NewMsg) (*pb.Msg, error) {
-	log.Printf("Received: %v, %v, %v, %v, %v", in.GetContent(), in.GetFrom(), in.GetUsername(), in.GetIdentifier(), in.GetHash());
-	s3Data := s3credentials.GetS3Data("frontend", "local", "")
-	log.Printf("S3 data %v", s3Data)
-	jobId := uuid.NewString()
-	return &pb.Msg{Content: in.GetContent(), From: in.GetFrom(), JobId: jobId }, nil
+	if reflect.TypeOf(in.GetContent()).Kind() == reflect.String &&
+	reflect.TypeOf(in.GetFrom()).Kind() == reflect.String &&
+	reflect.TypeOf(in.GetUsername()).Kind() == reflect.String &&
+	reflect.TypeOf(in.GetIdentifier()).Kind() == reflect.String &&
+	reflect.TypeOf(in.GetHash()).Kind() == reflect.String {
+		if len(in.GetContent()) > 0 && len(in.GetFrom()) > 0 && len(in.GetUsername()) > 0 && len(in.GetIdentifier()) > 0 && len(in.GetHash()) > 0 {
+			log.Printf("Received: %v, %v, %v, %v, %v", in.GetContent(), in.GetFrom(), in.GetUsername(), in.GetIdentifier(), in.GetHash())
+			s3Data := s3credentials.GetS3Data("mongo", "u", "")
+			log.Printf("S3 data %v", s3Data)
+			// Access mongo and check user identifier against hash to determine if request should be honoured 
+			jobId := uuid.NewString()
+			return &pb.Msg{Content: in.GetContent(), From: in.GetFrom(), JobId: jobId }, nil
+		}
+	}
+	err := errors.New("Request to Sms Service failed")
+	return &pb.Msg{Content: "Bad Request", From: "Tycoon Services", JobId: "null" }, err
 }
 
 func main() {
@@ -32,7 +46,6 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
 	}
-
 	s := grpc.NewServer()
 	pb.RegisterSmsManagementServer(s, &SmsManagementServer{})
 	log.Printf("Server listening at %v", lis.Addr())
