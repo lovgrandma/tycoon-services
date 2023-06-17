d9934b3e5c0579c3b97bd1bfe4c64d93687e8c16
Jesse <jessealexanderthompson@gmail.com>
2023-06-17 18:20:33 -0400

    no escape

--- tycoon_services.go
+++ tycoon_services.go
@@ -359,7 +359,7 @@ func handleIngestLiveStreamPublishAuthentication(w http.ResponseWriter, r *http.
 				// Handle the stream ingestion logic here
 				redirectStream := streamingServer + "/stream/?domain=" + domain + "&key=" + name + "&input=" + streamKey
 				log.Printf("Redirect Stream %v", redirectStream)
-				http.Redirect(w, r, url.PathEscape(redirectStream), http.StatusSeeOther)
+				http.Redirect(w, r, redirectStream, http.StatusSeeOther)
 
 				// response := fmt.Sprintf("Name: %s, Domain: %s", name, domain)
 				// w.WriteHeader(http.StatusOK)
